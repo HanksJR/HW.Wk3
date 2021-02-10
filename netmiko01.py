84c0e46ec5cc6bf2872cc632ea8feca0d70f4e2c
NetAcad DEVASC <changeme@example.com>
2021-02-10 11:59:31 +0000

    config routing,interface and loopback

--- netmiko01.py
+++ netmiko01.py
@@ -21,7 +21,27 @@ R1 = {
                         'ip': "172.20.182.4",
                         'netmask': "255.255.255.255"
                     }
-                ]
+                ],
+        'routing':[
+                    {
+                        'protocol': 'ospf 1',
+                        'network': '172.31.182.16',
+                        'wildcard': '0.0.0.15',
+                        'area' : '0'
+                    },
+                    {
+                        'protocol': 'ospf 1',
+                        'network': '172.31.182.32',
+                        'wildcard': '0.0.0.15',
+                        'area' : '0'
+                    },
+                    {
+                        'protocol': 'ospf 1',
+                        'network': '172.20.182.4',
+                        'wildcard': '0.0.0.0',
+                        'area' : '0'
+                    },
+        ]
     }
 
 R2 = {
@@ -42,7 +62,27 @@ R2 = {
                         'ip': "172.20.182.5",
                         'netmask': "255.255.255.255"
                     }
-                ]
+                ],
+        'routing':[
+                    {
+                        'protocol': 'ospf 1',
+                        'network': '172.31.182.16',
+                        'wildcard': '0.0.0.15',
+                        'area' : '0'
+                    },
+                    {
+                        'protocol': 'ospf 1',
+                        'network': '172.31.182.48',
+                        'wildcard': '0.0.0.15',
+                        'area' : '0'
+                    },
+                    {
+                        'protocol': 'ospf 1',
+                        'network': '172.20.182.5',
+                        'wildcard': '0.0.0.0',
+                        'area' : '0'
+                    },
+        ]
     }
 
 R3 = {
@@ -68,7 +108,33 @@ R3 = {
                         'ip': "172.20.182.6",
                         'netmask': "255.255.255.255"
                     }    
-                ]
+                ],
+        'routing':[
+                    {
+                        'protocol': 'ospf 1',
+                        'network': '172.31.182.32',
+                        'wildcard': '0.0.0.15',
+                        'area' : '0'
+                    },
+                    {
+                        'protocol': 'ospf 1',
+                        'network': '172.31.182.48',
+                        'wildcard': '0.0.0.15',
+                        'area' : '0'
+                    },
+                    {
+                        'protocol': 'ospf 1',
+                        'network': '172.31.182.64',
+                        'wildcard': '0.0.0.15',
+                        'area' : '0'
+                    },
+                    {
+                        'protocol': 'ospf 1',
+                        'network': '172.20.182.6',
+                        'wildcard': '0.0.0.0',
+                        'area' : '0'
+                    },
+        ]
     }   
 R4 = {
         'device_ip': '172.31.182.7',
@@ -83,7 +149,21 @@ R4 = {
                         'ip': "172.20.182.7",
                         'netmask': "255.255.255.255"
                     }
-                ]
+                ],
+        'routing':[
+                    {
+                        'protocol': 'ospf 1',
+                        'network': '172.31.182.64',
+                        'wildcard': '0.0.0.15',
+                        'area' : '0'
+                    },
+                    {
+                        'protocol': 'ospf 1',
+                        'network': '172.20.182.7',
+                        'wildcard': '0.0.0.0',
+                        'area' : '0'
+                    },
+        ]
     }
         
 
@@ -97,23 +177,37 @@ R5 = {
                     },
                     {
                         'interface': 'g0/2',
-                        'ip': 'dhcp'
+                        'ip': 'dhcp',
+                        'netmask': ''
                     },
-                    ,
                     {
                         'interface': "lo0",
                         'ip': "172.20.182.9",
                         'netmask': "255.255.255.255"
                     }
-                ]
+                ],
+        'routing':[
+                    {
+                        'protocol': 'ospf 1',
+                        'network': '172.31.182.64',
+                        'wildcard': '0.0.0.15',
+                        'area' : '0'
+                    },
+                    {
+                        'protocol': 'ospf 1',
+                        'network': '172.20.182.9',
+                        'wildcard': '0.0.0.0',
+                        'area' : '0'
+                    },
+        ]
     }
 
 all_device = [R1, R2, R3, R4, R5]
 
 def main():
-    config_interface_all(device_ip)
     for dev in all_device:
         config_interface(dev)
+        routing(dev)
     
 def config(iface, ip, netmask):
     interface_config = [
@@ -148,4 +242,35 @@ def show_interface_brief(ip):
         interface = ssh.send_command('sh ip int br')
         ssh.disconnect()
     return interface
+
+def routing(device):
+    ip = device["device_ip"]
+
+    for route in device["routing"]:
+        routing = [
+            "router {}".format(route["protocol"]),
+            "network {} {} area {}".format(route["network"], route["wildcard"], route["area"])]
+        
+        device_params = {'device_type': 'cisco_ios',
+                        'ip': ip,
+                        'username': username,
+                        'password': password,
+                        }
+
+        with ConnectHandler(**device_params) as ssh:
+            ssh.send_config_set(routing)
+            ssh.disconnect()
+
+    show_ip_route(ip)
+
+def show_ip_route(ip):
+    device_params = {'device_type': 'cisco_ios',
+                        'ip': ip,
+                        'username': username,
+                        'password': password,
+                        }
+    with ConnectHandler(**device_params) as ssh:
+        print(ssh.send_command('sh ip route'))
+        ssh.disconnect()
+
 main()
