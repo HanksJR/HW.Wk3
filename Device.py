29ba847b94c74491239389d6ccf506d45b5f284d
JokerS <43026670+HanksJR@users.noreply.github.com>
2021-02-17 01:31:27 +0700

    update
    create class, module and organize

---
+++ Device.py
@@ -0,0 +1,36 @@
+import os, yaml, create_config_file
+from netmiko import ConnectHandler
+
+class Device:
+
+    def __init__(self, username, password, path, ip):
+        self.username = username
+        self.password = password
+        self.device = self.create_list(path)
+        self.ip = ip
+        create_config_file.create()
+
+    def create_list(self, path):
+        lis = []
+        for file in os.listdir(path):
+            if file.endswith(".txt"):
+                lis.append(file)
+        return lis
+
+    def config(self, device, ip):
+        counter = 0
+        for dev in self.device:
+            device_params = {'device_type': 'cisco_ios',
+                                'ip': self.ip[counter],
+                                'username': self.username,
+                                'password': self.password,
+                                }
+            with ConnectHandler(**device_params) as ssh:
+                ssh.send_config_from_file(dev)
+                info = ssh.send_command("sh run")
+                devices = yaml.load(open('config_info.yaml'), Loader=yaml.FullLoader)
+                self.write_file(info, "information/{}_info.txt".format(devices[counter]["name"]))
+
+    def write_file(self, info, name):
+        with open(name, "w") as f:
+            f.write(info)
--- create_config_file.py
+++ create_config_file.py
@@ -1,13 +1,14 @@
 from jinja2 import Environment, FileSystemLoader
 import yaml
 
-env = Environment(loader=FileSystemLoader('template'), trim_blocks=True, lstrip_blocks=True)
-template = env.get_template('manage_router.txt')
+def create():
+    env = Environment(loader=FileSystemLoader('template'), trim_blocks=True, lstrip_blocks=True)
+    template = env.get_template('manage_router.txt')
 
-devices = yaml.load(open('config_info.yaml'), Loader=yaml.FullLoader)
+    devices = yaml.load(open('config_info.yaml'), Loader=yaml.FullLoader)
 
-path = 'router_info/'
-for device in devices:
-    config_file = path + device['name'] + '_config.txt'
-    with open(config_file, 'w') as f:
-        f.write(template.render(device))
+    path = 'router_info/'
+    for device in devices:
+        config_file = path + device['name'] + '_config.txt'
+        with open(config_file, 'w') as f:
+            f.write(template.render(device))
