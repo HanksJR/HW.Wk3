c659695355aae92dd81d9340052905b59f1b81e4
JokerS <43026670+HanksJR@users.noreply.github.com>
2021-02-17 16:49:18 +0700

    update

--- Device.py
+++ Device.py
@@ -2,13 +2,14 @@ import os, yaml, create_config_file
 from netmiko import ConnectHandler
 
 class Device:
-
+    
     def __init__(self, username, password, path, ip):
         self.username = username
         self.password = password
         self.device = self.create_list(path)
         self.ip = ip
         create_config_file.create()
+        create_config_file.delete()
         self.config(self.device, self.ip)
 
     def create_list(self, path):
@@ -19,6 +20,20 @@ class Device:
         return lis
 
     def config(self, device, ip):
+        counter = 0
+        for dev in self.device:
+            device_params = {'device_type': 'cisco_ios',
+                                'ip': self.ip[counter],
+                                'username': self.username,
+                                'password': self.password,
+                                }
+            with ConnectHandler(**device_params) as ssh:
+                ssh.send_config_from_file(dev)
+                info = ssh.send_command("sh run")
+                devices = yaml.load(open('config_info.yaml'), Loader=yaml.FullLoader)
+                self.write_file(info, "information/{}_info.txt".format(devices[counter]["name"]))
+
+    def delete_config(self, device, ip):
         counter = 0
         for dev in self.device:
             device_params = {'device_type': 'cisco_ios',
--- netmiko01.py
+++ netmiko01.py
@@ -1,9 +1,10 @@
 from Device import Device
 
-device_ip = ["172.31.182.3", "172.31.182.4", "172.31.182.5", "172.31.182.6", "172.31.182.7", "172.31.182.8", "172.31.182.9"]
+device_ip = ["172.31.182.4", "172.31.182.5", "172.31.182.6", "172.31.182.7", "172.31.182.9", "172.31.182.3", "172.31.182.8"]
 username = "admin"
 password = "cisco"
-path = "router_info"
+path = " routr_info"
 
 if __name__ == "__main__":
     d = Device(username, password, path, device_ip)
+    
