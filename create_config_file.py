751549406e5c853f9020d5e37c4e3b901dc334f6
JokerS <43026670+HanksJR@users.noreply.github.com>
2021-02-17 14:14:23 +0700

    function delete config

--- create_config_file.py
+++ create_config_file.py
@@ -12,3 +12,17 @@ def create():
         config_file = path + device['name'] + '_config.txt'
         with open(config_file, 'w') as f:
             f.write(template.render(device))
+
+def delete():
+    env = Environment(loader=FileSystemLoader('template'), trim_blocks=True, lstrip_blocks=True)
+    template = env.get_template('delete_template.txt')
+
+    devices = yaml.load(open('config_info.yaml'), Loader=yaml.FullLoader)
+
+    path = 'delete_config/'
+    for device in devices:
+        config_file = path + device['name'] + '_delete_config.txt'
+        with open(config_file, 'w') as f:
+            f.write(template.render(device))
+
+delete()
