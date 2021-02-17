ba0cec936e8d4af1fbc11e645596d92039d0ff5b
JokerS <43026670+HanksJR@users.noreply.github.com>
2021-02-18 01:12:05 +0700

    Update netmiko01.py

--- netmiko01.py
+++ netmiko01.py
@@ -3,7 +3,7 @@ from Device import Device
 device_ip = ["172.31.182.4", "172.31.182.5", "172.31.182.6", "172.31.182.7", "172.31.182.9", "172.31.182.3", "172.31.182.8"]
 username = "admin"
 password = "cisco"
-path = " routr_info"
+path = " router_info"
 
 if __name__ == "__main__":
     d = Device(username, password, path, device_ip)
